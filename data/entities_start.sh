00ee9b5fd32cab6b106e1e83e140833cdeb9ebd0
David Campo <dncampo@gmail.com>
2024-09-17 01:13:16 +0200

    Add test files, Dockerfile backend creation

--- data/entities_start.sh
+++ data/entities_start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-
-
-
-
 # Main execution
 echo "Starting entity and subscription creation..."
 # Execute subscription.sh script
-./subscription.sh
-./entities_dataset.sh
 ./entities_model.sh
+./subscriptions.sh
+./entities_dataset.sh
+
 
 echo "Entity and subscription creation completed."
